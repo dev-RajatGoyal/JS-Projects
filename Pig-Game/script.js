d6ef402b77e0029dfa140d7adfae3e77c07a8271
@rajat.goyal <rajat.goyal@enwidth.com>
2022-04-12 23:17:24 +0530

    bugs fixed

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -11,9 +11,6 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-
 let scores, currentScore, activePlayer, playing;
 
 const init = function () {
